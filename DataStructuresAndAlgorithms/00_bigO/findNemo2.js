11af3c9cccdfb29bf46297a591e936cffae45c8f
DAltier <altierdana@gmail.com>
2024-08-09 22:04:53 -0700

    big o exercise

--- DataStructuresAndAlgorithms/00_bigO/findNemo2.js
+++ DataStructuresAndAlgorithms/00_bigO/findNemo2.js
@@ -15,14 +15,11 @@ const everyone = [
 const large = new Array(10).fill('nemo');
 
 function findNemo2(fish) {
-  let t0 = performance.now();
   for (let i = 0; i < fish.length; i++) {
     if (fish[i] === 'nemo') {
       console.log('Found NEMO!');
     }
   }
-  let t1 = performance.now();
-  console.log('Call to find Nemo took ' + (t1 - t0) + ' milliseconds.');
 }
 
-findNemo2(everyone);
+findNemo2(everyone); // O(n) --> Linear Time
